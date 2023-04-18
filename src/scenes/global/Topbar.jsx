466cb9c9c3b146b75aee2581e7f380bb5a29d14c
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-04-18 18:29:36 +0500

    Basic Setup
    Added structure, color theme, and basic typography setup.

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,13 @@
+import {Box, IconButton, useTheme} from '@mui/material';
+import { useContext } from 'react';
+import InputBase from '@mui/material';
+import { ColorModeContext, tokens} from '../../theme';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+
+const Topbar = () => {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar
