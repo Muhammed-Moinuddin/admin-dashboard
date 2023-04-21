021e5ee632ca617d3ce915f4db80be40434e0f9f
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-04-21 06:59:14 +0500

    Topbar Completed
    Just made topbar and started working on Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calender from "./scenes/calender";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,8 +22,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline/>
         <div className="app">
-          <main className="context">
+          <Sidebar/>
+          <main className="content">
             <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+              {/* <Route path="/" element={<Team/>} /> */}
+              {/* <Route path="/" element={<Contacts/>} /> */}
+              {/* <Route path="/" element={<Invoices/>} /> */}
+              {/* <Route path="/" element={<Form/>} /> */}
+              {/* <Route path="/" element={<Bar/>} /> */}
+              {/* <Route path="/" element={<Pie/>} /> */}
+              {/* <Route path="/" element={<Line/>} /> */}
+              {/* <Route path="/" element={<FAQ/>} /> */}
+              {/* <Route path="/" element={<Geography/>} /> */}
+              {/* <Route path="/" element={<Calender/>} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useState} from "react";
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import 'react-pro-sidebar/dist/css/styles.css';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import {HomeOutlinedIcon, PeopleOutlinedIcon, ContactsOutlinedIcon, ReceiptOutlinedIcon, PersonOutlinedIcon, CalenderOutlinedIcon, HelpOutlinedIcon, BarChartOutlinedIcon, PieChartOutlinedIcon, TimelineOutlinedIcon, MenuOutlinedIcon, MapOutlinedIcon} from '@mui/icons-material';
 
 const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setCollapsed] = useState(false);
+  const [selected, setSelected] = useState('Dashboard');
   return (
-    <div>Sidebar</div>
+    <Box
+      sx={{
+        "& .pro-sidebar-inner" : {
+
+        }
+      }}
+    >
+
+    </Box>
   )
 }
 
